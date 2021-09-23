36c3c3c88747c7bcf43624bff789b9109a317a6d
JeanFrancoLeon <jfranco93@byui.edu>
2021-09-23 05:42:19 -0500

    prev

--- js/dates.js
+++ js/dates.js
@@ -2,5 +2,9 @@ const copyrightyear = document.getElementById("copyrightyear");
 copyrightyear.textContent = new Date().getFullYear();
 
 
+const lastModified = Date.parse(document.lastModified)
+      document.write('<p>Last updated: '  + document.lastModified);
+
+
 //document.getElementById("copyrightyear").textContent = new Date().getFullYear();;
 //document.querySelector("#copyrightyear").textContent = new Date().getFullYear();;
