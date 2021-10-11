51c9547400f8a529395ef909e0164a4ea669dbde
JeanFrancoLeon <jfranco93@byui.edu>
2021-10-11 04:02:10 -0500

    www

---
+++ lesson4/js/dates.js
@@ -0,0 +1,7 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+document.querySelector("#lastupdated").textContent = `Last Updated: ${document.lastModified}`;
+
+
+
+
