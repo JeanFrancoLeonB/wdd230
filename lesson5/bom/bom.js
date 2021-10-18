dada4b0c44b9eee3262d956684ab833102123a6e
JeanFrancoLeon <jfranco93@byui.edu>
2021-10-17 20:49:02 -0500

    chang

---
+++ lesson5/bom/bom.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('input');
+        const button = document.querySelector('button');
+        const list = document.querySelector('.list');
+        const main = document.querySelector('main');
+        const p = document.createElement('p');
+
+        button.addEventListener('click', function() {
+            let userInput =  input.value;
+
+            if (userInput == ""){
+                p.textContent ='Please enter a Book of Mormon chapter.';
+                main.appendChild(p);
+                p.style.display = 'block';
+        }
+            
+            else {    
+                const listItem = document.createElement('li');
+                const deleteBtn = document.createElement('button');
+                listItem.textContent = userInput;
+                deleteBtn.textContent = "\u274C";
+                listItem.appendChild(deleteBtn);
+                list.appendChild(listItem);
+                console.log(p);
+                p.style.display = 'none';
+                deleteBtn.addEventListener('click',function() {
+                list.removeChild(listItem);    
+            })
+            }
+        input.focus();
+        input.value = "";
+        })
