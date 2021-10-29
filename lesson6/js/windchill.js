989c90e9909358f1d9cb40d674da2c4df1c393e7
JeanFrancoLeon <jfranco93@byui.edu>
2021-10-29 17:30:38 -0500

    chan

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,13 +1,10 @@
-var t= document.getElementById("temp").textContent;
-var s= document.getElementById("windSpeed").textContent;
-
-function windChill(t,s) {
-  if((t<= 50) && (s>=3)){
-    var output = Math.round((35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s,0.16))) + (0.4275 * (t*(Math.pow(s,0.16))))) + "F";
-  } else{
-    var output = "N/A";
-  }
-  return output;
+const t = parseFloat(document.querySelector('.temp').textContent);
+const s = parseFloat(document.querySelector('.speed').textContent);
+let f;
+if (t <= 50 && s >= 3) {
+  f = `${(35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16))).toFixed(0)}°F`;
 }
-
-document.getElementById("windChill").textContent = windChill(t,s);
+else {
+  f = "N/A";
+}
+document.querySelector('.wind').textContent = f;
