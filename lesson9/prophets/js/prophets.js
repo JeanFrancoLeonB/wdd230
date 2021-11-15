d2a86ca22aacc1380008053ee217be65d455216c
JeanFrancoLeon <jfranco93@byui.edu>
2021-11-15 01:14:17 -0500

    sss

--- lesson9/prophets/js/prophets.js
+++ lesson9/prophets/js/prophets.js
@@ -6,7 +6,7 @@ fetch(requestURL)
   .then(function (jsonObject) {
     const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
-      let card = document.createElement('section');
+      let prophetscards = document.createElement('section');
       let name = document.createElement('h2');
       let birthDate = document.createElement('p');
       let birthPlace = document.createElement('p');
@@ -16,10 +16,10 @@ fetch(requestURL)
       birthPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
       portrait.setAttribute('src', prophets[i].imageurl);
       portrait.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + '-' + ' ' + prophets[i].order);
-      card.appendChild(name);
-      card.appendChild(birthDate);
-      card.appendChild(birthPlace);
-      card.appendChild(portrait);
-      document.querySelector('div.cards').appendChild(card);
+      prophetscards.appendChild(name);
+      prophetscards.appendChild(birthDate);
+      prophetscards.appendChild(birthPlace);
+      prophetscards.appendChild(portrait);
+      document.querySelector('div.prophetscards').appendChild(prophetscards);
     }
   });
